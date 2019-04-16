a5874c1aa165bc150e421b07bc9263942b1c4921
Alan Savio Paul <alan.s.paul09@gmail.com>
2019-04-16 11:09:20 +0100

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Address-Book-App
-Address Book with Sqlite3
+# ADDRESS-BOOK
+
+An address book project created with the use of basic frontend technologies + Pug and SQLite.
+
+So far the app allows you to add addresses, and then edit and remove one or all of them. Once you add an address, an SQLite file will be created to store all the data.
+
+Commands to run the project: npm install, node app.js
+
+Then go to http://localhost:3000/ in your browser.
+
+![screen](/screen.png)
---
+++ app.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var sqlite3 = require('sqlite3').verbose();
+var pug = require('pug');
+
+var db = new sqlite3.Database('database/db');
+var app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+    res.sendFile('public/index.html', {'root': __dirname});
+});
+
+app.post('/getdata', function(req, res) {
+    var templateUpdated = '';
+    db.each("SELECT rowid, * FROM addressData ORDER BY rowid DESC", function(err, row) {
+        var addressData = row;
+        var fn = pug.compileFile('./views/new-addresses.pug');
+        templateUpdated = templateUpdated + fn({ addressData });
+    }, function() {
+        res.send( templateUpdated );
+    });
+});
+
+app.post('/newaddress', function(req, res) {
+    var addressData = {
+        name: req.body.name,
+        phone: req.body.phone,
+        address: req.body.address
+    }
+
+    db.serialize(function() {
+        db.run("INSERT INTO addressData (name, phone, address) VALUES (?, ?, ?)", (addressData.name), (addressData.phone), (addressData.address));
+        db.each("SELECT rowid, * FROM addressData WHERE rowid=(SELECT MAX(rowid) FROM addressData)", function(err, row) {
+            addressData.rowid = row.rowid;
+            var fn = pug.compileFile('./views/new-address.pug');
+            var templateUpdated = fn({ addressData });
+            res.send( {templateUpdated: templateUpdated, rowid: addressData.rowid} );
+        });
+    });
+});
+
+app.post('/deleteaddress', function(req, res) {
+    var idToDelete = req.body.idToDelete;
+    db.run("DELETE FROM addressData WHERE rowid=?", idToDelete);
+    res.send('The address was deleted from the db.');
+});
+
+app.post('/deleteaddresses', function(req, res) {
+    db.run("DELETE FROM addressData");
+    res.send('All addresses were deleted from the db.');
+});
+
+app.post('/updateaddress', function(req, res) {
+    var dataToUpdate = req.body;
+    db.run("UPDATE addressData SET name=?, phone=?, address=? WHERE rowid=?", dataToUpdate.nameUpdated, dataToUpdate.phoneUpdated, dataToUpdate.addressUpdated, dataToUpdate.idToUpdate);
+    res.send('The address was updated in the db.');
+});
+
+app.listen(3000, function() {
+    console.log('The app is listening on port 3000!');
+    db.run("CREATE TABLE IF NOT EXISTS addressData (rowid INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, phone TEXT, address TEXT)"); // text?
+});
